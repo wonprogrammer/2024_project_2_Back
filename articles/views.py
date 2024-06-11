a44db0888d16830a3f99ae3f6f42936bf7a1e461
leehyewon <tnddl2345@gmail.com>
2024-06-11 19:33:40 +0900

    ADD url,views > API 확인

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from articles.models import Article
 
 # Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    articles = Article.objects.all()
+    article = articles[0]
+    article_data = {
+        'title':article.title,
+        'content':article.content,
+    }
+    return Response(article_data)

9b01497a03e79b41bfacb75d5a217684dcb5312a
leehyewon <tnddl2345@gmail.com>
2024-06-18 18:46:10 +0900

    ADD articleDetailAPI, Modify urls

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from articles import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.articleAPI, name='articleAPI'),
+    path('<int:article_id>/', views.articleDetailAPI, name='articleDetailAPI'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from articles.models import Article
 from articles.serializers import ArticleSerializer
 
@@ -18,7 +19,7 @@ from articles.serializers import ArticleSerializer
 #     return Response(article_data)
 
 @api_view(['GET', 'POST'])
-def index(request):
+def articleAPI(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
@@ -32,3 +33,22 @@ def index(request):
             print(serializer.error_messages)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             # return Response(serializer.error_messages) | 보안상 이유로 Front에서 어떤 이유로 에러가 났는지 확인이 가능하기 때문에 해당 코드는 지양
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def articleDetailAPI(request, article_id):
+    if request.method == 'GET':
+        # article = Article.objects.get(id = article_id) | 아래 코드와 동일한 의미 + 잘못된 요청일때 404 상태 return
+        article = get_object_or_404(Article, id = article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        article = get_object_or_404(Article, id = article_id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    elif request.method == 'DELETE':
+        article = get_object_or_404(Article, id = article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
